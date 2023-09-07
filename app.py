3e99cfca6a7c41f17b0d9d364d0915697dcfa05d
sooryong <sooryong.byun@gmail.com>
2023-09-07 09:20:30 +0900

    using pytube instead of pafy

--- app.py
+++ app.py
@@ -64,28 +64,28 @@ if source_radio == settings.IMAGE:
             st.error("Error occurred while opening the image.")
             st.error(ex)
     with col2:
-        if source_img is None:
-            default_detected_image_path = str(settings.DEFAULT_DETECT_IMAGE)
-            default_detected_image = PIL.Image.open(
-                default_detected_image_path)
-            st.image(default_detected_image_path, caption='Detected Image',
-                     use_column_width=True)
-        else:
-            if st.sidebar.button('Detect Objects'):
-                res = model.predict(uploaded_image, conf=confidence)
-                boxes = res[0].boxes
-                res_plotted = res[0].plot()[:, :, ::-1]
-                st.image(res_plotted, caption='Detected Image', use_column_width=True)
-                try:
-                    with st.expander("Detection Results"):
-                        for box in boxes:
-                            st.write(box.data)
-                except Exception as ex:
-                    # st.write(ex)
-                    st.write("No image is uploaded yet!")
+        # if source_img is None:
+        #     default_detected_image_path = str(settings.DEFAULT_DETECT_IMAGE)
+        #     default_detected_image = PIL.Image.open(
+        #         default_detected_image_path)
+        #     st.image(default_detected_image_path, caption='Detected Image',
+        #              use_column_width=True)
+        # else:
+        if st.sidebar.button('Detect Objects'):
+            res = model.predict(uploaded_image, conf=confidence)
+            boxes = res[0].boxes
+            res_plotted = res[0].plot()[:, :, ::-1]
+            st.image(res_plotted, caption='Detected Image', use_column_width=True)
+            try:
+                with st.expander("Detection Results"):
+                    for box in boxes:
+                        st.write(box.data)
+            except Exception as ex:
+                # st.write(ex)
+                st.write("No image is uploaded yet!")
 
-elif source_radio == settings.VIDEO:
-    helper.play_stored_video(confidence, model)
+# elif source_radio == settings.VIDEO:
+#     helper.play_stored_video(confidence, model)
 
 elif source_radio == settings.YOUTUBE:
     helper.play_youtube_video(confidence, model)
